d464e85f71b0d485f2176f0e79a3fc8da527ce6f
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-03-31 16:40:04 +0000

    Remove redefinition of  `__sync_fetch_and_add_2`

--- source/core/codal_default_target_hal.cpp
+++ source/core/codal_default_target_hal.cpp
@@ -1,7 +1,7 @@
-#include "codal_target_hal.h"
-#include "CodalDmesg.h"
 #include "CodalCompat.h"
+#include "CodalDmesg.h"
 #include "Timer.h"
+#include "codal_target_hal.h"
 
 __attribute__((weak)) void target_wait(uint32_t milliseconds)
 {
@@ -28,8 +28,7 @@ __attribute__((weak)) void target_panic(int statusCode)
     target_disable_irq();
 
     DMESG("*** CODAL PANIC : [%d]", statusCode);
-    while (1)
-    {
+    while (1) {
     }
 }
 
@@ -37,30 +36,4 @@ __attribute__((weak)) void target_deepsleep()
 {
     // if not implemented, default to WFI
     target_wait_for_event();
-}
-
-/**
- * Default implementation of atomic fetch and add opertaion.
- * GCC provides this where possible, but this is not supported on some CPU architectures...
- *
- * @param ptr pointer to the memory to access.
- * @param value the value to add to the memory location.
- * @return the value of th ememory location BEFORE the add operation took place.
- */
-__attribute__((weak)) short unsigned int __sync_fetch_and_add_2 (volatile void *ptr, short unsigned int value)
-{
-
-#if CONFIG_ENABLED(DISABLE_IRQ_FOR_SOFTWARE_ATOMICS)
-    target_disable_irq();
-#endif
-
-    uint16_t *p = (uint16_t *)ptr;
-    uint16_t old = *p;
-    *p += value;
-
-#if CONFIG_ENABLED(DISABLE_IRQ_FOR_SOFTWARE_ATOMICS)
-    target_enable_irq();
-#endif
-
-    return old;
 }
