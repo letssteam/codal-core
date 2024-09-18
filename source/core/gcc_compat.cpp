a33b71be952dd53b020fe6c601ac963210ceef79
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2024-09-18 14:20:39 +0000

    Put weak attribute on function definition and not declaration

--- source/core/gcc_compat.cpp
+++ source/core/gcc_compat.cpp
@@ -1,42 +1,43 @@
 #include "gcc_compat.h"
-#include "codal_target_hal.h"
+
 #include "CodalAssert.h"
+#include "codal_target_hal.h"
 
 #if __GNUC__ > 11
 extern "C" {
-    int _close( int fd )
-    {
-        assert_fault( "Newlib syscalls are not supported!" );
-        return -1;
-    }
+__attribute__((weak)) int _close(int fd)
+{
+    assert_fault("Newlib syscalls are not supported!");
+    return -1;
+}
 
-    int _getpid()
-    {
-        assert_fault( "Newlib syscalls are not supported!" );
-        return -1;
-    }
+__attribute__((weak)) int _getpid()
+{
+    assert_fault("Newlib syscalls are not supported!");
+    return -1;
+}
 
-    int _kill(int pid, int sig)
-    {
-        assert_fault( "Newlib syscalls are not supported!" );
-        return -1;
-    }
+__attribute__((weak)) int _kill(int pid, int sig)
+{
+    assert_fault("Newlib syscalls are not supported!");
+    return -1;
+}
 
-    int _lseek(int file, int ptr, int dir)
-    {
-        assert_fault( "Newlib syscalls are not supported!" );
-        return -1;
-    }
+__attribute__((weak)) int _lseek(int file, int ptr, int dir)
+{
+    assert_fault("Newlib syscalls are not supported!");
+    return -1;
+}
 
-    int _read(int file, char *ptr, int len)
-    {
-        assert_fault( "Newlib syscalls are not supported!" );
-        return -1;
-    }
-    int _write(int file, char *ptr, int len)
-    {
-        assert_fault( "Newlib syscalls are not supported!" );
-        return -1;
-    }
+__attribute__((weak)) int _read(int file, char* ptr, int len)
+{
+    assert_fault("Newlib syscalls are not supported!");
+    return -1;
+}
+__attribute__((weak)) int _write(int file, char* ptr, int len)
+{
+    assert_fault("Newlib syscalls are not supported!");
+    return -1;
+}
 }
 #endif
